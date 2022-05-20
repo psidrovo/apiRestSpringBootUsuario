be6b7c79d6c4791ad2e9e695dbd09c1bdb7d2b2b
pidrovo <paulsidrovob@gmail.com>
2022-05-20 01:14:59 -0500

    Aplicacion funcionando con los metodos REST usando Get y Post

---
+++ src/main/java/ec/ups/edu/demoPersona/DemoPersonaApplication.java
@@ -0,0 +1,13 @@
+package ec.ups.edu.demoPersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/ups/edu/demoPersona/DemoPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ups.edu.demoPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
